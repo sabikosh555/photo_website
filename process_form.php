08d17bd7890365369042d880bb0749725fa36843
sabikosh555 <sabinokka0520@gmail.com>
2024-11-17 15:04:19 +0500

    Delete process_form.php

--- process_form.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] = "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $age = (int)$_POST['age'];
-
-    echo "Your name: $name<br>" ;
-    echo "Your age: $age" ;
-}
